41d8fdcdff4da7ddd5ece255fd71cd9579bb7e0b
munawir4 <munawir4@gmail.com>
2021-09-24 11:31:36 +0530

    new

---
+++ resources/views/table.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border=10 align=center width=500 height=100>
+    <tr>
+        <th><u>psg</u></th>
+        <th><u>city</u></th>
+    </tr>
+    <tr>
+<td>10</td>
+<td>21</td>
+    </tr>
+    <tr>
+        <td>22</td>
+        <td>32</td>
+    </tr>
+    <tr>
+        <td>23</td>
+        <td>56</td>
+    </tr>
+    </table>
+    
+<br>
+<br>
+<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. <div> Voluptatum modi illo similique doloribus quis nostrum! Laboriosam numquam maxime quas odio et voluptatibus perferendis sapiente dicta nobis similique. Sed, quam maxime.</div>
+</div>Lorem ipsum dolor, sit amet consectetur adipisicing elit.>Expedita ea quo quisquam earum totam pariatur rem rerum, praesentium minima labore reiciendis ullam facere nostrum vel veritatis minus illo fugiat ratione.
+</body>
+</html>
